77fad61d408747d6bd3fa3952711165fec676e91
SilentFlick <brickflick@protonmail.com>
2022-11-24 21:57:35 +0100

    Feat: add restapi for db and ldap

--- src/backend/pbo.sql
+++ src/backend/pbo.sql
@@ -1,4 +1,4 @@
-create table posts (
+create table if not exists posts (
     post_id integer primary key autoincrement,
     from_user integer,
     to_user integer,
@@ -8,8 +8,17 @@ create table posts (
     foreign key(to_user) references professor_lecturer(pl_id) on delete cascade
 );
 
-create table professor_lecturer (
+create table if not exists professor_lecturer (
     pl_id integer primary key autoincrement,
     name varchar (255) not null,
     faculty varchar (50) not null
 );
+
+insert into professor_lecturer values (null, 'Sebastian Aland', 'Informatik');
+insert into professor_lecturer values (null, 'Jürgen Anke', 'Informatik');
+insert into professor_lecturer values (null, 'Robert Baumgartl', 'Informatik');
+insert into professor_lecturer values (null, 'Arnold Beck', 'Informatik');
+insert into professor_lecturer values (null, 'Kai Bruns', 'Informatik');
+insert into professor_lecturer values (null, 'Georg Freitag', 'Informatik');
+
+insert into posts values (null,2,null,'Krankmeldung','Leider muss die LV für heute absagen');
