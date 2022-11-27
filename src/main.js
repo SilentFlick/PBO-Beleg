1ce879d63fbce83ae0e7ff7ec30a2a514f22a704
qduyp <92373381+qduyp@users.noreply.github.com>
2022-11-27 11:15:00 +0100

    Create: HomePage

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).mount("#app");
+
+import "bootstrap/dist/js/bootstrap.js";
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  root: path.resolve(__dirname, "./"),
+
+  server: {
+    port: 8080,
+    hot: true,
+  },
+  plugins: [vue()],
+});
