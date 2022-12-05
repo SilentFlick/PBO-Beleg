4d7109fa86e312937ae8f193ef47dfa39a054988
qduyp <92373381+qduyp@users.noreply.github.com>
2022-12-05 21:13:27 +0100

    reupload navbar

---
+++ src/components/SideBarLeft.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="d-flex flex-column flex-shrink-0 p-3 bg-dark col-2 align-items-center sidebar-left"
+  >
+    <ul
+      class="nav nav-pills flex-column position-fixed float-left"
+      style="width: 150px"
+    >
+      <li class="nav-item">
+        <a href="/" class="nav-link text-white active" aria-current="page">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="/post" class="nav-link text-white link-dark">
+          Create a Post
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white link-dark"> Selection </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white link-dark"> Q&A </a>
+      </li>
+      <li>
+        <a href="/about" class="nav-link text-white link-dark">
+          Help/Support
+        </a>
+      </li>
+    </ul>
+    <hr />
+  </div>
+</template>
+<style scoped>
+.sidebar-left {
+  height: 100vh;
+}
+</style>
---
+++ src/components/TopNavBar.vue
@@ -0,0 +1,42 @@
+<script setup></script>
+
+<template>
+  <!-- Top NavBar -->
+  <header class="navbar sticky-top">
+    <nav class="container-fluid flex-wrap">
+      <a class="navbar-brand">HTW Shits Talk</a>
+      <form class="" role="search">
+        <input
+          class="form-control"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+      </form>
+
+      <button class="btn btn-info btn-lg" type="button">Login</button>
+    </nav>
+  </header>
+</template>
+<style scoped>
+header {
+  margin: 0;
+  padding: 0 30px;
+  width: 100%;
+  background-color: #f99b1c;
+}
+nav {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+button {
+  padding: 10px 30px;
+}
+.form-control {
+  margin: 10px;
+  width: 500px;
+  height: 50px;
+}
+</style>
