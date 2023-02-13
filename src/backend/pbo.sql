5404c1cb6fb4ae382ab0a27f86981686783bef34
Br0kenD3ath <111383197+Br0kenD3ath@users.noreply.github.com>
2023-02-13 20:39:54 +0100

    Br0ken d3ath patch 1 (#21)
    Co-authored-by: SilentFlick <87384243+SilentFlick@users.noreply.github.com>

--- src/backend/pbo.sql
+++ src/backend/pbo.sql
@@ -26,17 +26,44 @@ create table if not exists professor_lecturer (
     faculty varchar (50) not null
 );
 
+insert into professor_lecturer values (null, 'Sebastian Aland', 'Informatik');
+insert into professor_lecturer values (null, 'Jürgen Anke', 'Informatik');
+insert into professor_lecturer values (null, 'Robert Baumgartl', 'Informatik');
+insert into professor_lecturer values (null, 'Arnold Beck', 'Informatik');
+insert into professor_lecturer values (null, 'Kai Bruns', 'Informatik');
+insert into professor_lecturer values (null, 'Georg Freitag', 'Informatik');
+
+insert into professor_lecturer values (null, 'Engel Jens', 'Bauingenieurwesen');
+insert into professor_lecturer values (null, 'Krimmling Jörn', 'Bauingenieurwesen');
+insert into professor_lecturer values (null, 'Kunze Undine', 'Bauingenieurwesen');
+
+insert into professor_lecturer values (null, 'Mathiebe Elke', 'Design');
+insert into professor_lecturer values (null, 'Baese Jenny', 'Design');
+insert into professor_lecturer values (null, 'Scholz Christian', 'Design');
+
+insert into professor_lecturer values (null, 'Binner Andreas', 'Elektrotechnik');
+insert into professor_lecturer values (null, 'Schmidt Marc-Peter ', 'Elektrotechnik');
+insert into professor_lecturer values (null, 'Zipser, Stephan', 'Elektrotechnik');
+
+insert into professor_lecturer values (null, 'Kowanda Andreas', 'Geoinformation');
+insert into professor_lecturer values (null, 'Zimmermann Jörg', 'Geoinformation');
+insert into professor_lecturer values (null, 'Müller Martina', 'Geoinformation');
+
+insert into professor_lecturer values (null, 'Freick Markus ', 'Landbau/Umwelt/Chemie');
+insert into professor_lecturer values (null, 'Lux Guido ', 'Landbau/Umwelt/Chemie');
+insert into professor_lecturer values (null, 'Schmidtke Knut', 'Landbau/Umwelt/Chemie');
+
+insert into professor_lecturer values (null, 'Göbel Gunther', 'Maschinenbau');
+insert into professor_lecturer values (null, 'Naumann Gunther', 'Maschinenbau');
+insert into professor_lecturer values (null, 'Römhild Iris', 'Maschinenbau');
+
+insert into professor_lecturer values (null, 'Ortmanns Wolfgang', 'Wirtschaftingenieurwesen');
+insert into professor_lecturer values (null, 'Sonntag Ralph', 'Wirtschaftingenieurwesen');
+insert into professor_lecturer values (null, 'Gestring Ingo', 'Wirtschaftingenieurwesen');
 
 
-insert into professor_lecturer values (null, 'Sebastian Aland', 'Informatik/Mathematik');
-insert into professor_lecturer values (null, 'Jürgen Anke', 'Informatik/Mathematik');
-insert into professor_lecturer values (null, 'Robert Baumgartl', 'Informatik/Mathematik');
-insert into professor_lecturer values (null, 'Arnold Beck', 'Informatik/Mathematik');
-insert into professor_lecturer values (null, 'Kai Bruns', 'Informatik/Mathematik');
-insert into professor_lecturer values (null, 'Georg Freitag', 'Informatik/Mathematik');
-
-INSERT INTO posts(post_id, from_user, to_user, faculty, title, content) VALUES(null, 2, null,'Informatik/Mathematik' , 'Krankmeldung', 'Leider muss die LV für heute absagen');
-INSERT INTO posts(post_id, from_user, to_user, faculty, title, content) VALUES(null, null, null,'Informatik/Mathematik' , 'Funny story in today class', 'Laughter is infectious. It lightens the weights we carry in life, uplifts our moods, and bonds us to those we share in it with. So why would not we embrace any chance we have to giggle at a joke? That is why we have rounded up that set of (clean) jokes for adults and kids alike that will have the whole family laughing.
+INSERT INTO posts(post_id, from_user, to_user, faculty, title, content) VALUES(null, 2, null,'Informatik' , 'Krankmeldung', 'Leider muss die LV für heute absagen');
+INSERT INTO posts(post_id, from_user, to_user, faculty, title, content) VALUES(null, null, null,'Informatik' , 'Funny story in today class', 'Laughter is infectious. It lightens the weights we carry in life, uplifts our moods, and bonds us to those we share in it with. So why would not we embrace any chance we have to giggle at a joke? That is why we have rounded up that set of (clean) jokes for adults and kids alike that will have the whole family laughing.
 But that is not all. In addition to the 70 jokes below, we have also got dad jokes, jokes for kiddos, mom jokes, and jokes for holidays that you can share them with the youngest person in the room.
 Get ready: Some of what is to come is quite punny. Some might even make your eyes roll. But, deep down, if we are honest, who does not smile at corny jokes? Others might even make you laugh so hard you cry, so do not say we did not warn you. Many are one-liners so you can remember them to share and share again, and your kids can retell them to their friends too, maybe even years later. Now get ready to make some memories filled with laughter with these 70 hilariously funny jokes!');
 INSERT INTO posts(post_id, from_user, to_user, faculty, title, content) VALUES(null, 3, null,'Informatik/Mathematik', 'Hausaufgaben für Weihnachten', 'Randomly creates a first and last name, using an array of common first and last names. GenerateName() returns a string[2] (2 element array) containing the randomly chosen first and last name. This is written as a class so it can be instantiated and called numerous times when creating large datasets.');
